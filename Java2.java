7c879313504b7607862e991450dbefc81b949d48
jayanth288 <chintu78299@gmail.com>
2022-06-10 22:21:54 -0700

    It commit

---
+++ Java2.java
@@ -0,0 +1,26 @@
+public class Java2{
+    public static void main(String[]args){
+// String s = "GOOD";
+// String b = "Dinner";
+// System.out.println(s + b);
+// String s = "GOOD";
+// char c = 'B';
+// System.out.println(s + c);
+// String s = "GOOD";
+// int  b = 4;
+// String result = s + b;
+// System.out.println(result);
+System.out.println("Enter two numbers");
+int a = 8;
+int b = 4;
+int result = a + b;
+System.out.println(a + b);
+System.out.println(a == b);
+System.out.println(a != b);
+System.out.println(a <= b);
+System.out.println(a >= b);
+System.out.println(a > b);
+System.out.println(a < b);
+
+    }
+}
