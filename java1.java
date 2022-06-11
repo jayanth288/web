e498cd9d42a2fe85d19e19cc0cdab591ad99bb6d
jayanth288 <chintu78299@gmail.com>
2022-06-10 22:14:39 -0700

    Int commit

---
+++ java1.java
@@ -0,0 +1,37 @@
+public class java1 {
+  public static void main(String[] args) {
+    // int a = 5;
+    // int b = 18;
+    // System.out.println(a + b);
+    // double a = 0.9;
+    // double b = 47.04;
+    // System.out.println(a + b);
+    String s;
+    s = "Jayanth"; //Name//
+    String d; 
+   d = "08/01/2000"; //dob
+    String q; 
+    q = " Lovely professional university"; //college
+    int r; 
+    r = 2022; //yop
+    int w;
+    w = 22; //age
+    int z;
+    z = 6; //cgpa
+    boolean b;
+    b = false; //backlogstatus
+    String h; 
+    h= "B+";
+    String j;
+    j = "Alive";
+    System.out.println(s);
+    System.out.println(d);
+    System.out.println(q);
+    System.out.println(r);
+    System.out.println(w);
+    System.out.println(z);
+    System.out.println(b);
+    System.out.println(h);
+    System.out.println(j);
+  }
+}
